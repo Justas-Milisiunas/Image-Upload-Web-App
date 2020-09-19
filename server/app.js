2e71a8e2329e4dd897abfdb7c717c6d1b53906e3
JUSTAS-MILISIUNAS <justas.milisiunas@gmail.com>
2020-09-19 17:41:43 +0300

    Fixed issue with images rounting not working

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/images', imagesRouter);
 
 app.listen(PORT, () => console.log(`Server started, listening at: ${PORT}`));
