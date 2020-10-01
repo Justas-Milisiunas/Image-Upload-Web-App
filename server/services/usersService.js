ccb87e50173e23004745d62bbed5f8f8da4665a1
JUSTAS-MILISIUNAS <justas.milisiunas@gmail.com>
2020-10-01 10:22:10 +0300

    Implemented user endpoints

---
+++ server/services/usersService.js
@@ -0,0 +1,48 @@
+const User = require('../models/user');
+
+module.exports.getUsers = async () => User.find();
+
+module.exports.getUser = async (id) => {
+    try {
+        return await User.findById(id);
+    } catch (e) {
+        console.log(e.message);
+        return null;
+    }
+}
+
+module.exports.createUser = async (email, password) => {
+    let newUser = new User({
+        email,
+        password
+    });
+
+    const error = newUser.validateSync();
+    if (error) {
+        console.log(error.message);
+        return;
+    }
+
+    return await newUser.save();
+}
+
+module.exports.deleteUser = async (id) => {
+    try {
+        return await User.findByIdAndDelete(id);
+    } catch (e) {
+        console.log(e.message)
+        return null;
+    }
+}
+
+module.exports.updateUser = async (id, newUserData) => {
+    try {
+        return await User.findByIdAndUpdate(id, newUserData, {
+            new: true
+        });
+    } catch (e) {
+        console.log(e.message);
+        return null;
+    }
+}
+
